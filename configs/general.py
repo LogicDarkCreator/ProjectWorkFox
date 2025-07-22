8983be6be3ef36125433d17e0eeb78767e346272
JSD Dev Portal <logicdarkcreator@gmail.com>
2025-07-22 13:34:59 +0300

    Add files via upload

---
+++ configs/general.py
@@ -0,0 +1,15 @@
+##
+ # Config for men gender
+##
+
+Height = 170
+RecSteps = 5000
+Step1 = Height * 0.415
+
+##
+ # Config for women gender
+##
+
+Height2 = 165
+RecSteps2 = 5000
+Step2 = Height * 0.413
